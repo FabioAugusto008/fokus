fd3225f2eb10736cd509f3e6c870dcfb816ec138
Fabio <facarvalho235@gmail.com>
2026-08-19 19:55:49 -0300

    4.6

--- app/index.jsx
+++ app/index.jsx
@@ -1,22 +1,58 @@
+import { useState } from "react";
 import { Image } from "react-native";
 import { StyleSheet, Text, View, Pressable } from "react-native";
+import { FokusButton } from "../components/FokusButton";
+import { ActionButton } from "../components/ActionButton";
+
+const pomodoro = [
+  {
+    id: 'focus',
+    initialValue: 25,
+    image: require('./pomodoro.png'),
+    display: 'Foco'
+  },
+  {
+    id: 'short',
+    initialValue: 5,
+    image: require('./short.png'),
+    display: 'Pausa Curta'
+  },
+  {
+    id: 'long',   
+    initialValue: 15,
+    image: require('./long.png'),
+    display: 'Pausa Longa'
+  },
+]
 
 export default function Index() {
+const [timerType, setTimerType] = useState(pomodoro[0])
+
   return (
     <View
       style={styles.container}
     >
-      <Image source={require('./pomodoro.png')}/>
+      <Image source={timerType.image}/>
       <View style={styles.actions}> 
+        <View style={styles.context}>
+          {pomodoro.map(p => (
+            <ActionButton 
+              key={p.id}
+              active={ timerType.id === p.id }
+              onPress={() => setTimerType(p)}
+              display={p.display}
+            />
+          ))}
+        </View>
+
         <Text style={styles.timer}>
-          25:00
+          { new Date(timerType.initialValue * 1000).toLocaleTimeString('pt-BR', { minute: '2-digit', second: '2-digit'}) }
         </Text>
-        <Pressable style={styles.button}>
-          <Text style={styles.buttonText}> 
-            Começar
-          </Text>
-        </Pressable>
+
+        <FokusButton />
+
       </View>
+
       <View style={styles.footer}>
         <Text style={styles.footerText}>
           Projeto fictício e sem fins comerciais. 
@@ -25,6 +61,7 @@ export default function Index() {
           Desenvolvido por Alura.
         </Text>
       </View>
+
     </View>
   );
 }
@@ -45,21 +82,16 @@ export default function Index() {
       borderColor: '#144480',
       gap: 32
     },
+    context: {
+      flexDirection: 'row',
+      justifyContent: 'space-evenly',
+      alignItems: 'center',
+    },
     timer: {
       fontSize: 54,
       color: '#FFF',
       fontWeight: 'bold',
       textAlign: 'center',
-    },
-    button: {
-      backgroundColor: '#B872FF',
-      borderRadius: 32,
-      padding: 8,
-    },
-    buttonText: {
-      textAlign: 'center',
-      color: '#021123',
-      fontSize: 18,
     },
     footer: {
       width: '80%'
@@ -68,6 +100,5 @@ export default function Index() {
       color: '#98A0A8',
       fontSize: 12.5,
       textAlign: 'center',
-      
     }
   })
---
+++ components/FokusButton/index.jsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Pressable, Text } from "react-native"
+
+export const FokusButton = () => {
+    return(
+        <Pressable style={styles.button}>
+            <Text style={styles.buttonText}> 
+                Começar
+            </Text>
+        </Pressable>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: '#B872FF',
+        borderRadius: 32,
+        padding: 8,
+    },
+    buttonText: {
+        textAlign: 'center',
+        color: '#021123',
+        fontSize: 18,
+    },
+})
