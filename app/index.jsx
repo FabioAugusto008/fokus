838f7b4dd6259da950d902a6dbf8a1b477777be2
Fabio <facarvalho235@gmail.com>
2026-08-20 14:16:30 -0300

    new curso, 1 - check

--- app/index.jsx
+++ app/index.jsx
@@ -1,144 +1,58 @@
-import { useState } from "react";
-import { Image } from "react-native";
-import { StyleSheet, Text, View, Pressable } from "react-native";
+import { Link } from "expo-router";
+import { Text, Image, View } from "react-native";
 import { FokusButton } from "../components/FokusButton";
-import { ActionButton } from "../components/ActionButton";
-import { Timer } from "../components/Timer";
-import { useRef } from "react";
-import { IconPause, IconPlay } from "../components/Icons";
-
-const pomodoro = [
-  {
-    id: 'focus',
-    initialValue: 25 * 60,
-    image: require('./pomodoro.png'),
-    display: 'Foco'
-  },
-  {
-    id: 'short',
-    initialValue: 5 * 60,
-    image: require('./short.png'),
-    display: 'Pausa Curta'
-  },
-  {
-    id: 'long',   
-    initialValue: 15 * 60,
-    image: require('./long.png'),
-    display: 'Pausa Longa'
-  },
-] 
+import { StyleSheet } from "react-native";
 
 export default function Index() {
-
-const [timerType, setTimerType] = useState(pomodoro[0])
-
-const [timerRunning, setTimerRunning] = useState(false)
-
-const [seconds, setSeconds] = useState(pomodoro[0].initialValue)
-
-const timerRef = useRef(null)
-
-const clear = () => {
-  if (timerRef.current != null) {
-    clearInterval(timerRef.current)
-    timerRef.current = null
-    setTimerRunning(false)
-  }
-}
-
-const toggleTimerType = (newTimerType) => {
-  setTimerType(newTimerType)
-  setSeconds(newTimerType.initialValue)
-  clear()
-}
-
-const toggleTimer = () => {
-  if (timerRef.current) {
-    clear()
-    return
-  }
-
-  setTimerRunning(true)
-
-  const id = setInterval(() => {
-    setSeconds(oldState => {
-      if (oldState === 0 ){
-        clear()
-        return timerType.initialValue
-      }
-      return oldState - 1
-    })
-  }, 1000)
-  timerRef.current = id
-}
-
-  return (
-    <View
-      style={styles.container}
-    >
-      <Image source={timerType.image}/>
-      <View style={styles.actions}> 
-        <View style={styles.context}>
-          {pomodoro.map(p => (
-            <ActionButton 
-              key={p.id}
-              active={ timerType.id === p.id }
-              onPress={() => toggleTimerType(p)}
-              display={p.display}
+    return (<View style={styles.container}>
+        <Image source={require("../assets/images/logo.png")} />
+        <View style={styles.inner}>
+            <Text style={styles.title}>
+                Otimize sua {'\n'} produtividade, {'\n'}
+                <Text style={styles.bold}>
+                    mergulhe no que {'\n'} importa
+                </Text>
+            </Text>
+            <Image source={require('../assets/images/home.png')} />
+            <FokusButton 
+                title="Quero iniciar!"
+                onPress={() => console.log('navegar')}
             />
-          ))}
         </View>
-
-        <Timer totalSeconds={seconds} />
-
-        <FokusButton 
-          title={timerRunning ? 'Pausar' : 'Começar'}
-          icon={timerRunning ? <IconPause /> : <IconPlay />}
-          onPress={toggleTimer}
-        />
-
-      </View>
-
-      <View style={styles.footer}>
-        <Text style={styles.footerText}>
-          Projeto fictício e sem fins comerciais. 
-        </Text>
-        <Text style={styles.footerText}>
-          Desenvolvido por Alura.
-        </Text>
-      </View>
-
-    </View>
-  );
+        <View style={styles.footer}>
+            <Text style={styles.footerText}>
+                Projeto fictício e sem fins comerciais.
+            </Text>
+            <Text style={styles.footerText}>Desenvolvido por Alura.</Text>
+        </View>
+    </View>);
 }
-  const styles = StyleSheet.create({
+
+const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: '#021121',
-      gap: 40
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#021121",
+        gap: 40,
     },
-    actions: {
-      padding: 24,
-      backgroundColor: '#14448080',
-      width: '80%',
-      borderRadius: 32,
-      borderWidth: 2,
-      borderColor: '#144480',
-      gap: 32
+    inner: {
+        gap: 16
     },
-    context: {
-      flexDirection: 'row',
-      justifyContent: 'space-evenly',
-      alignItems: 'center',
+    title: {
+        color: '#FFF',
+        textAlign: 'center',
+        fontSize: 26
+    },
+    bold: {
+        fontWeight: 'bold',
     },
     footer: {
-      width: '80%'
+        width: "80%",
     },
     footerText: {
-      color: '#98A0A8',
-      fontSize: 12.5,
-      textAlign: 'center',
-    }
-  })
+        color: "#98A0A8",
+        fontSize: 12.5,
+        textAlign: "center",
+    },
+});
